c6f65b0b5dc584544ea17a3ae37617797c60f756
Schaeffer91604 <ejschae@clemson.edu>
2025-10-12 14:50:48 -0400

    Created post request in admin

--- backend/admin-service/server.js
+++ backend/admin-service/server.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,3 +10,20 @@ app.use('/api', routes);
 
 const PORT = 5001;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
+const createEvent = () => {
+    const [name, setName] = useState('');
+    const [date, setDate]= useState('');
+    const [tickets, setTickets] = useState('');
+
+    const eventSubmit = (event) => {
+        event.preventDefault();
+        const newEvent = {name, date, tickets};
+
+        fetch('http://localhost:5000/api/events', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newEvent)
+        })
+    }
+}
